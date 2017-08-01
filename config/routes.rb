807890b0a625dda8221e434d2a7ffe30ebf29d67
Daniel.Newbury <Daniel.Newbury@Shopify.com>
2017-08-01 11:04:23 -0400

    added Sessions controller, login routes, new session view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'login' => :destroy
+  end
+
+
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+
   resources :requests
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
